08812a911a315bec8ad7252cfbed74089eb0f578
Brian Schwinn <bschwinn@gmail.com>
2014-07-04 12:08:24 -0400

    define templates and a default view for app.html

--- libmaelstrom/views.py
+++ libmaelstrom/views.py
@@ -3,6 +3,11 @@ from flask import make_response, jsonify, url_for
 from libmscope import app, db
 
 
+@app.route('/', methods = ['GET'])
+def get_app():
+	return render_template("app.html", title = 'Maelstrom - A BrewPi Interface')
+
+
 @app.route('/appsettings', methods = ['GET'])
 def get_appsettings():
     session = db.DBSession()
