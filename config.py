4634ebad8e251bc2cc3ae128961a95cec532dd0d
Brian Schwinn <bschwinn@gmail.com>
2014-07-04 11:02:31 -0400

    remove junk in config file

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-#SQLALCHEMY_DATABASE_URI = 'mysql://mscope:mscope@localhost/mscope'
 SQLALCHEMY_DATABASE_URI = os.path.dirname(os.path.realpath(__file__))+"/data"
 
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
