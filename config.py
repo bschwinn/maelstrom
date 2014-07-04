da111cd1872d2045071284b5b00b76da7036ee9b
Brian Schwinn <bschwinn@gmail.com>
2014-07-04 11:06:41 -0400

    fix connect string for Flask

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = os.path.dirname(os.path.realpath(__file__))+"/data"
+SQLALCHEMY_DATABASE_URI = "sqlite:///"+ basedir + "/data/db"
 
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
